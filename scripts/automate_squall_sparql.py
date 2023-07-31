21423ecbb774e481b56899d7ac753c329e4168fb
dhananjaybhandiwad <91888251+dhananjaybhandiwad@users.noreply.github.com>
2023-07-31 21:08:25 +0200

    Update automate_squall_sparql.py
    removed print statements.

--- scripts/automate_squall_sparql.py
+++ scripts/automate_squall_sparql.py
@@ -9,12 +9,9 @@ def squall2sparql(input_final_path, output_file_path):
 	with open(output_file_path, 'w') as f:
 		for x in input_list:
 			x = x.strip("\n").strip()
-			print(x)
 			p = subprocess.Popen(['../tools/squall2sparql.sh',"-wikidata",x], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			out, err = p.communicate()
 			if out.decode() == "":
 				f.write(f"{err.decode()}\n")
-				print(err.decode())
 			else:
 				f.write(f"{out.decode()}")
-				print(out.decode().replace("\n",""))
